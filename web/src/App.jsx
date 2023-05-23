dc805504b769aea2d7a5339716cdceb987013b4c
ArslanYM <arsalanyaqmalik@gmail.com>
2023-05-23 17:47:20 +0530

    fix in app.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home, About, Contributors } from "./pages";
+import { Home, About } from "./pages";
 import MasterLayout from "./layouts";
 
 function App() {
@@ -11,7 +11,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contributors" element={<Contributors />} />
         </Routes>
       </BrowserRouter>
     </MasterLayout>
