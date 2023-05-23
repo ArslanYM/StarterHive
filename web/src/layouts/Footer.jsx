346903cb8889166ff7d4b5da9760b0ddd816eae5
Arsalan Yaqoob Malik <104521101+ArslanYM@users.noreply.github.com>
2023-05-23 11:02:25 +0530

    Update Footer.jsx

--- web/src/layouts/Footer.jsx
+++ web/src/layouts/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
                     <span className="ml-3 text-xl">StarterHive</span>
                 </a>
                 <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© 2023 StarterHive —
-                    <a href="https://twitter.com/knyttneve" className="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">@ArslanYM</a>
+                    <a href="https://twitter.com/ArsalanYmalik" className="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">@ArslanYM</a>
                 </p>
                 <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                     <a className="text-gray-400">
