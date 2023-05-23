19cc70b676f2e9ef0ad92e6b3d7c339a20ac6ff1
Kanchan253 <kanchandaryanani253@gmail.com>
2023-05-23 19:06:28 +0530

    made changes to the header

--- web/src/layouts/Header.jsx
+++ web/src/layouts/Header.jsx
@@ -1,28 +1,45 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import "./header.css"; // Import the CSS file
 
 const Header = () => {
-    return (
-        <header className="text-gray-400 bg-gray-900 body-font">
-            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-                    <a className="mr-5 hover:text-white">Home</a>
-                    <a className="mr-5 hover:text-white">Contributors</a>
-                    <a className="mr-5 hover:text-white">Docs</a>
-                    <a className="hover:text-white">Find Issues</a>
-                </nav>
-                <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-white lg:items-center lg:justify-center mb-4 md:mb-0">
+  const titleRef = useRef(null);
 
-                    <span className="ml-3 text-xl xl:block lg:hidden">Starter Hive✨</span>
-                </a>
-                <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
-                    <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Github
-                    </button>
-                </div>
-            </div>
-        </header>
+  useEffect(() => {
+    const titleElement = titleRef.current;
+    titleElement.classList.add("fade-in");
 
-    )
-}
+    return () => {
+      titleElement.classList.remove("fade-in");
+    };
+  }, []);
+
+  return (
+    <header className="text-gray-400 bg-gray-900 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
+          <a className="mr-5 hover:text-white">Home</a>
+          <a className="mr-5 hover:text-white">Contributors</a>
+          <a className="mr-5 hover:text-white">Docs</a>
+          <a className="hover:text-white">Find Issues</a>
+        </nav>
+        <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-white lg:items-center lg:justify-center mb-4 md:mb-0">
+          <span
+            ref={titleRef}
+            className="ml-3 text-4xl xl:block font-bold lg:hidden text-white"
+          >
+            Starter Hive✨
+          </span>
+          {/* <span className="ml-3 text-xl xl:block lg:hidden font-bold text-white">Starter Hive<span className="text-yellow-400">✨</span></span> */}
+        </a>
+        <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+          <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+            Github
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
