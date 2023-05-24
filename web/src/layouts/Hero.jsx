8d950b72d6752fe70feb254fc69b5f7eab9c69b8
Muhammad Rabi <vonrabi07@yahoo.com>
2023-05-24 23:41:50 +0300

    Fix whitespace under footer

--- web/src/layouts/Hero.jsx
+++ web/src/layouts/Hero.jsx
@@ -1,36 +1,31 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
-import PropTypes from "prop-types";
 import heroPicture from "../assets/hero_picture.png";
 
-const Hero = ({ children }) => {
+const Hero = () => {
   return (
     <>
-      <section className="text-gray-400 bg-gray-900 body-font">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <div>{children}</div>
-            <h1 className="title-font sm:text-5xl text-4xl mb-4 font-extrabold text-white">
-              Contribute to
-              <br className="hidden lg:inline-block" /> the world of Open Source
-            </h1>
-            <p className="font-bold mb-8 leading-relaxed">
-              Starter Hive is a platform that helps beginners to contribute to
-              open source projects of remotely hiring organizations.
-            </p>
-            <div className="flex justify-center">
-              <button className="inline-flex text-white bg-purple-500 border-0 py-3 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg font-bold">
-                Find Issues
-              </button>
-            </div>
+      <section className="flex flex-col items-center pt-12 md:pt-0 min-h-[80vh] px-5 mx-auto text-gray-400 bg-gray-900 body-font md:flex-row">
+        <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0">
+          <h1 className="mb-4 text-4xl font-extrabold text-white title-font sm:text-5xl">
+            Contribute to
+            <br className="hidden lg:inline-block" /> the world of Open Source
+          </h1>
+          <p className="mb-8 font-bold leading-relaxed">
+            Starter Hive is a platform that helps beginners to contribute to
+            open source projects of remotely hiring organizations.
+          </p>
+          <div className="flex justify-center">
+            <button className="inline-flex px-8 py-3 text-lg font-bold text-white bg-purple-500 border-0 rounded focus:outline-none hover:bg-purple-600">
+              Find Issues
+            </button>
           </div>
-          <div className="lg:max-w-md lg:w-full md:w-1/2 w-5/6">
-            <img
-              className="object-cover object-center rounded"
-              alt="hero"
-              src={heroPicture}
-            />
-          </div>
+        </div>
+        <div className="w-5/6 lg:max-w-md lg:w-full md:w-1/2">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src={heroPicture}
+          />
         </div>
       </section>
     </>
@@ -38,7 +33,3 @@ const Hero = ({ children }) => {
 };
 
 export default Hero;
-
-Hero.propTypes = {
-  children: PropTypes.element.isRequired,
-};
