764f939618c5a2847a2b5af322d7e88185b05e0c
Jaisal Srivastava <48391316+JaiS99@users.noreply.github.com>
2023-05-24 13:56:33 +0530

    Added in icons with links as requested by issue 58. 	modified:   CONTRIBUTORS.md 	modified:   web/src/layouts/Footer.jsx

--- web/src/layouts/Footer.jsx
+++ web/src/layouts/Footer.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-
+import {FaDiscord,FaGithub,FaTwitter} from 'react-icons/fa'
 const Footer = () => {
     return (
         <footer className="text-gray-400 bg-gray-900 body-font">
@@ -15,27 +15,14 @@ const Footer = () => {
                     <a href="https://twitter.com/ArsalanYmalik" className="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">@ArslanYM</a>
                 </p>
                 <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                    <a className="text-gray-400">
-                        <svg fill="currentColor" className="w-5 h-5" viewBox="0 0 24 24">
-                            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                        </svg>
+                    <a href="https://twitter.com/ArsalanYmalik" className="text-gray-400">
+                        <FaTwitter />
                     </a>
-                    <a className="ml-3 text-gray-400">
-                        <svg fill="currentColor" className="w-5 h-5" viewBox="0 0 24 24">
-                            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                        </svg>
+                    <a href="https://github.com/ArslanYM/StarterHive" className="ml-3 text-gray-400">
+                        <FaGithub />
                     </a>
-                    <a className="ml-3 text-gray-400">
-                        <svg fill="none" stroke="currentColor" className="w-5 h-5" viewBox="0 0 24 24">
-                            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                        </svg>
-                    </a>
-                    <a className="ml-3 text-gray-400">
-                        <svg fill="currentColor" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-                            <circle cx="4" cy="4" r="2" stroke="none"></circle>
-                        </svg>
+                    <a href="https://discord.gg/QBwU8KaJ" className="ml-3 text-gray-400">
+                        <FaDiscord /> 
                     </a>
                 </span>
             </div>
