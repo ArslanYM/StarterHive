4ac59c57d5720305260625e774f49619bca8b9fa
Gennadii Shpreer <intellegento@gmail.com>
2023-05-25 20:16:53 +0600

    feat: add target blanc for links

--- web/src/layouts/Footer.jsx
+++ web/src/layouts/Footer.jsx
@@ -16,13 +16,13 @@ const Footer = () => {
                     <a href="https://twitter.com/ArsalanYmalik" className="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">@ArslanYM</a>
                 </p>
                 <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                    <a href="https://twitter.com/ArsalanYmalik" className="text-gray-400">
+                    <a href="https://twitter.com/ArsalanYmalik" target="_blank" className="text-gray-400" rel="noreferrer">
                         <FaTwitter className="text-2xl duration-300 hover:fill-sky-500" />
                     </a>
-                    <a href="https://github.com/ArslanYM/StarterHive" className="ml-5 text-gray-400">
+                    <a href="https://github.com/ArslanYM/StarterHive" target="_blank" className="ml-5 text-gray-400" rel="noreferrer">
                         <FaGithub className="text-2xl duration-300 hover:fill-gray-600" />
                     </a>
-                    <a href="https://discord.gg/QBwU8KaJ" className=" ml-5 text-gray-400">
+                    <a href="https://discord.gg/QBwU8KaJ"target="_blank" className="ml-5 text-gray-400" rel="noreferrer">
                         <FaDiscord className="text-2xl duration-300 hover:fill-violet-500" /> 
                     </a>
                 </span>
