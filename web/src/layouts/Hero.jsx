3fc2e9f837d60e5ec31abac7a20ae3864eb2b36c
John Doe <tirunaharipandu@gmail.com>
2023-05-25 21:29:59 +0530

    text edited

--- web/src/layouts/Hero.jsx
+++ web/src/layouts/Hero.jsx
@@ -3,15 +3,18 @@ import React from 'react'
 import PropTypes from "prop-types";
 
 const Hero = ({ children }) => {
+    
     return (
         <>
             <section className="text-gray-400 bg-gray-900 body-font">
                 <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-                    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                    <div className="lg:flex-grow md:w-1/2 lg:pr-20 lg:pl-12 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                         <div>{children}</div>
-                        <h1 className="title-font sm:text-5xl text-4xl mb-4 font-extrabold text-white">Contribute to
-                            <br className="hidden lg:inline-block" /> the world of Open Source
+                        <h1 className="title-font sm:text-5xl text-4xl mb-4 font-extrabold text-white">
+                            Contribute to<br className="hidden lg:inline-block" /> the world of
+                            <span className="text-blue-500 font-roboto-slab font-extrabold ml-5">Open Source</span>
                         </h1>
+
                         <p className="mb-8 leading-relaxed">Starter Hive is a platform that helps beginners to contribute to open source projects of remotely hiring organizations.</p>
                         <div className="flex justify-center">
                             <button className="inline-flex text-white bg-purple-500 border-0 py-3 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg font-bold">Find Issues</button>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,8 +4,8 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
+    theme: {
+    },
+   
   plugins: [],
 }
