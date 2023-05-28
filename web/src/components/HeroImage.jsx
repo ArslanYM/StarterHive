8fa27c15dedd76e282850b46bb60a053cbd8642d
rahilansari261 <rahilansari261@gmail.com>
2023-05-28 17:24:29 +0530

    fix: floating divs gets sizes back

--- web/src/components/HeroImage.jsx
+++ web/src/components/HeroImage.jsx
@@ -1,11 +1,5 @@
 const HeroImg = ({ className, imgSrc, altText }) => {
-  return (
-    <img
-      className={`bg-[#ba5eba] object-cover object-center rounded border-none border-transparent h-72 min-w-[40%] ${className}`}
-      src={imgSrc}
-      alt={altText}
-    />
-  );
+  return <img className={`bg-[#ba5eba]  border-none border-transparent h-48 w-32 rounded-lg  ${className}`} src={imgSrc} alt={altText} />;
 };
 
 export default HeroImg;
