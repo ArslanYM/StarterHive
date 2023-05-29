dd51af460d163681f68dc907a03b6fae6e62ac6e
Sambhav Gupta <sambhavgupta0705@gmail.com>
2023-05-29 12:29:15 +0530

    fix: changed the footer icon

--- web/src/layouts/Footer.jsx
+++ web/src/layouts/Footer.jsx
@@ -1,15 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import {FaDiscord,FaGithub,FaTwitter} from 'react-icons/fa'
-
+import footer_icon from '../assets/hive.svg'
 const Footer = () => {
     return (
         <footer className="text-gray-400 bg-gray-900 body-font">
             <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-                <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-10 h-10 text-white p-2 bg-purple-500 rounded-full" viewBox="0 0 24 24">
-                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                    </svg>
+                <a className="flex title-font font-medium items-center md:justify-start justify-center text-white" >
+<img className='object-contain h-6 w-6' src={footer_icon}/>
                     <span className="ml-3 text-xl">StarterHive</span>
                 </a>
                 <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© 2023 StarterHive —
