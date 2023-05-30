6501ee6365815cf85cbbffd544d0186ee4dcb6d5
ArslanYM <arsalanyaqmalik@gmail.com>
2023-05-30 22:08:33 +0530

    feat: added bold text in the navbar

--- web/src/layouts/Header.jsx
+++ web/src/layouts/Header.jsx
@@ -11,21 +11,21 @@ const Header = () => {
     <header className="text-gray-400 bg-gradient-to-r from-gray-700 via-gray-900 to-black  body-font flex-nowrap">
       <div className="container mx-auto flex flex-wrap p-5  flex-col md:flex-row items-center justify-between">
         <nav className="flex  flex-wrap items-center text-base ">
-          <Link to="/" className={` px-3 py-1 rounded hover:text-white cursor-pointer ${path == "/" && "bg-gray-800  "}`}>
+          <Link to="/" className={`px-3 py-1 rounded hover:text-white cursor-pointer font-bold ${path == "/" && "bg-gray-800  "}`}>
             Home
           </Link>
-          <Link to="/contributors" className={` px-3 py-1 rounded hover:text-white cursor-pointer ${path == "/contributors" && "bg-gray-800  "}`}>
+          <Link to="/contributors" className={` px-3 py-1 rounded hover:text-white font-bold cursor-pointer ${path == "/contributors" && "bg-gray-800  "}`}>
             Contributors
           </Link>
-          <Link to="/docs" className={` px-3 py-1 rounded hover:text-white cursor-pointer ${path == "/docs" && "bg-gray-800  "}`}>
+          <Link to="/docs" className={` px-3 py-1 rounded hover:text-white font-bold cursor-pointer ${path == "/docs" && "bg-gray-800  "}`}>
             Docs
           </Link>
-          <Link to="/issues" className={`hover:text-white cursor-pointer px-3 py-1 rounded ${path == "/issues" && "bg-gray-800  "}`}>
+          <Link to="/issues" className={`hover:text-white cursor-pointer font-bold px-3 py-1 rounded ${path == "/issues" && "bg-gray-800  "}`}>
             Find Issues
           </Link>
         </nav>
         <Link to="/" className="flex order-first  title-font font-medium items-center text-white  mb-4 md:mb-0">
-          <span className="ml-3 text-xl   cursor-pointer">Starter Hive ✨</span>
+          <span className="ml-3 text-xl  font-bold cursor-pointer">Starter Hive ✨</span>
         </Link>
         <div className=" inline-flex ">
           <button className="inline-flex bg-gray-800 py-2 px-3 focus:outline-none duration-300 hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
