91b91cf16d709e0205793c88a66dabbf9f5f4193
Promise Dash <promisedash1607@gmail.com>
2023-06-04 00:43:55 +0530

    project name enhancement

--- web/src/layouts/Footer.jsx
+++ web/src/layouts/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <footer className="text-gray-400 bg-gradient-to-r from-gray-700 via-gray-900 to-black body-font">
             <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
                 <a className="flex title-font font-medium items-center md:justify-start justify-center text-white" >
-                    <span className="ml-3 text-xl">StarterHive</span>
+                    <span className="ml-3 text-xl">Starter Hive</span>
 <img className='object-contain h-6 w-6 ml-3 ' src={footer_icon}/>
                 </a>
                 <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© 2023 StarterHive —
