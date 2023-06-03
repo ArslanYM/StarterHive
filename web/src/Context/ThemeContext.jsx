dec51a2faf6ea50312be66259f4a86616a7fab82
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2023-06-03 15:02:31 -0600

    FEAT: Add dark/light mode to the whole webapp #177

--- web/src/Context/ThemeContext.jsx
+++ web/src/Context/ThemeContext.jsx
@@ -1,20 +1,31 @@
 import { useState, createContext } from "react"
 
 const ThemeContext = createContext()
+
 const SCREEN_THEME = {
 	Light_Theme: "bg_Light_Theme",
 	Dark_Theme: "bg_Dark_Theme",
 }
 
 const ThemeProvider = ({ children }) => {
-	const [theme] = useState(SCREEN_THEME.Light_Theme)
+	const [theme, setTheme] = useState(() => {
+		if (localStorage.getItem("Theme")) {
+			const theme_Selected = JSON.parse(localStorage.getItem("Theme"))
+			return theme_Selected === SCREEN_THEME.Light_Theme
+				? SCREEN_THEME.Dark_Theme
+				: SCREEN_THEME.Light_Theme
+		}
+		return SCREEN_THEME.Light_Theme
+	})
 
-	const handleTheme = () => {
-		alert("Hola")
+	const handleTheme = (checked) => {
+		//Checked -> Boolean
+		checked ? setTheme(SCREEN_THEME.Dark_Theme) : setTheme(SCREEN_THEME.Light_Theme)
+		localStorage.setItem("Theme", JSON.stringify(theme))
 	}
 
 	return (
-		<ThemeContext.Provider value={{ handleTheme, theme, SCREEN_THEME }}>
+		<ThemeContext.Provider value={{ handleTheme, theme }}>
 			{children}
 		</ThemeContext.Provider>
 	)
--- web/src/components/Switch/Switch.jsx
+++ web/src/components/Switch/Switch.jsx
@@ -1,9 +1,13 @@
 import { FaSun, FaMoon } from 'react-icons//fa'
 import './style.css'
+import { useContext } from 'react'
+import { ThemeContext } from '../../Context/ThemeContext'
 const Switch = () => {
+	const { handleTheme } = useContext(ThemeContext)
+
 	return (
 		<div>
-			<input type="checkbox" className="checkbox" id="checkbox" />
+			<input type="checkbox" className="checkbox" id="checkbox" onClick={(e) => handleTheme(e.target.checked)} />
 			<label htmlFor="checkbox" className="checkbox-label">
 				<FaSun className='fa-sun' />
 				<FaMoon className='fa-moon' />
