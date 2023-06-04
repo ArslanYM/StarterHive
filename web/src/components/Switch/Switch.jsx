4c15c265cc4e02fd70fdf6ae47b38b836f96141f
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2023-06-03 18:59:01 -0600

    FEAT: Add dark/light mode to the whole webapp #177

--- web/src/components/Switch/Switch.jsx
+++ web/src/components/Switch/Switch.jsx
@@ -4,7 +4,7 @@ import './style.css'
 // eslint-disable-next-line react/prop-types
 const Switch = ({ handleTheme, boxThemeChecked }) => {
 	return (
-		<div>
+		<div className='switch__Theme'>
 			<input type="checkbox" className="checkbox" id="checkbox" checked={boxThemeChecked} onChange={(e) => handleTheme(e.target.checked)} />
 			<label htmlFor="checkbox" className="checkbox-label">
 				<FaSun className='fa-sun' />
