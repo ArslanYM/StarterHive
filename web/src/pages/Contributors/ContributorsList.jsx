96bbd2db39f80c157d8e95c958b713c2435246d9
Mriganka <179turtleshells@gmail.com>
2023-06-19 10:47:13 +0530

    Added a search bar to the Contributors List

--- web/src/pages/Contributors/ContributorsList.jsx
+++ web/src/pages/Contributors/ContributorsList.jsx
@@ -2,15 +2,16 @@ import ContributorItem from './ContributorItem'
 import { useState, useEffect, useContext } from 'react';
 import ScaleLoader from 'react-spinners/ScaleLoader'
 import { ThemeContext } from '../../Context/ThemeContext';
+import SearchTerm from './search/Search';
 
 import axios from 'axios';
 
 const ContributorsList = () => {
     const [contributors, setContributors] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [search, setSearch] = useState("")
 
     const { current_theme } = useContext(ThemeContext).theme
-
     useEffect(() => {
         const fetchContributors = async () => {
             setLoading(true);
@@ -30,19 +31,26 @@ const ContributorsList = () => {
             setLoading(false);
         };
 
-            fetchContributors();
+        fetchContributors();
     }, []);
+
+    const filtered = contributors.filter((entry) =>
+        entry.login.toLowerCase().match(new RegExp(search.toLowerCase(), "g"))
+    );
 
     return (
         <div className="flex flex-wrap -m-2 justify-center items-center">
-            <ScaleLoader 
+            <ScaleLoader
                 loading={loading}
                 color={current_theme === 'dark' ? 'white' : 'black'}
             />
-
-            {contributors.map((contributor, index) => {
+            <SearchTerm setTerm={setSearch}/>
+            {
+                filtered.length?
+                filtered.map((contributor, index) => {
                 return <ContributorItem image={contributor.avatar_url} name={contributor.login} url={contributor.html_url} key={index} />
             })
+                :`No result for ${search}`
             }
         </div>
     );
---
+++ web/src/pages/Contributors/search/Search.jsx
@@ -0,0 +1,8 @@
+export default function SearchTerm({setTerm}){
+    return (
+        <div style={{width: "100%"}}>
+            <input type="text" onChange={(e)=>setTerm(e.target.value)} className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Search"/>
+            <br/>
+        </div>
+    )
+}
