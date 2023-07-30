2c3ddaa6fb4e7052e3eda1552eb66d9a1814986d
Benedict Kpaduwa <benedict.k@cyberbyte.com.ng>
2023-07-30 17:38:35 +0100

    Added more border radius to the search bar

--- web/src/pages/Contributors/search/Search.jsx
+++ web/src/pages/Contributors/search/Search.jsx
@@ -1,7 +1,7 @@
 export default function SearchTerm({setTerm}){
     return (
         <div style={{width: "100%"}} >
-            <input type="text" onChange={(e)=>setTerm(e.target.value)} className="block w-1/3 mx-auto rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Search"/>
+            <input type="text" onChange={(e)=>setTerm(e.target.value)} className="block w-1/3 mx-auto rounded-xl border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Search"/>
             <br/>
         </div>
     )
