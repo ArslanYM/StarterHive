e54165ca750fb6140bd0d3f58df3432c3463fbd0
love-sonkar <loveboss806@gmail.com>
2023-09-17 22:23:19 +0530

    fixes #332 changes in projectlist file changed items-center to place-items-center for align center

--- web/src/pages/Issues/ProjectList.jsx
+++ web/src/pages/Issues/ProjectList.jsx
@@ -6,7 +6,7 @@ import ProjectCard from "./ListOfOrgs/ProjectCard";
 const ProjectList = () => {
   return (
     <div className="mx-auto min-h-screen max-w-screen-2xl p-4 md:p-8 2xl:p-10">
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-4 xl:grid-cols-3 2xl:gap-7.5 bg-gradient-to-r from-gray-700 via-gray-900 to-black bg-animate items-center">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-4 xl:grid-cols-3 2xl:gap-7.5 bg-gradient-to-r from-gray-700 via-gray-900 to-black bg-animate place-items-center">
         {projectList.map((project, key) => {
           return (
             <ProjectCard
