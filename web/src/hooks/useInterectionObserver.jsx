e15336c188a176c9691f7f46791305fd57102358
surfiniaburger <jdmasciano2@gmail.com>
2023-09-22 18:04:34 +0100

    added comments to the hooks

--- web/src/hooks/useInterectionObserver.jsx
+++ web/src/hooks/useInterectionObserver.jsx
@@ -1,26 +1,37 @@
 import { useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
 
-export const useInterectionObserver = (margin = "0px") => {
+// Custom hook for IntersectionObserver
+export const useIntersectionObserver = (margin = "0px") => {
+  // State to track whether the observed element is intersecting
   const [isIntersecting, setIsIntersecting] = useState(false);
 
+  // Ref to hold a reference to the observed element
   const observerRef = useRef(null);
 
+  // Effect hook to set up the IntersectionObserver
   useEffect(() => {
+    // Create an IntersectionObserver
     const observer = new IntersectionObserver(
       ([entry]) => {
+        // Update isIntersecting based on the observed intersection
         setIsIntersecting(entry.isIntersecting);
       },
-      { rootMargin: margin }
+      { rootMargin: margin } // Apply margin options to the observer
     );
+
+    // Start observing the referenced element
     observer.observe(observerRef.current);
 
+    // Clean up the observer when the component unmounts
     return () => observer.disconnect();
-  }, [margin]);
+  }, [margin]); // Re-run the effect when the margin prop changes
 
+  // Return an object containing the isIntersecting state and observerRef
   return { isIntersecting, observerRef };
 };
 
-useInterectionObserver.propTypes = {
-  isVisible: PropTypes.string,
+// Prop type validation for the useIntersectionObserver hook
+useIntersectionObserver.propTypes = {
+  isVisible: PropTypes.string, // Prop type validation for the isVisible prop
 };
