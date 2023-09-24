67750eda5722bf6c2602080e6b0b63e0a7cea13c
Kaushik Tripathi <kaushiktripathi847@gmail.com>
2023-09-25 03:05:01 +0530

    Feat:Project Links open in new tab

--- web/src/pages/Issues/ListOfOrgs/ProjectCard.jsx
+++ web/src/pages/Issues/ListOfOrgs/ProjectCard.jsx
@@ -40,7 +40,7 @@ const ProjectCard = ({
 
   return (
     <div className="flex self-auto flex-col h-full max-w-sm border rounded-lg shadow bg-gray-800 border-gray-700">
-      <a href={projectLink}>
+      <a href={projectLink} target="_blank" rel="noopener noreferrer">
         <img
           rel="preload"
           loading="lazy"
@@ -54,7 +54,7 @@ const ProjectCard = ({
         />
       </a>
       <div className="grid grid-cols-1 h-full p-5">
-        <a href={projectLink}>
+        <a href={projectLink} target="_blank" rel="noopener noreferrer" >
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
         </a>
         <p className="mb-3 font-normal text-gray-400">{description}</p>
