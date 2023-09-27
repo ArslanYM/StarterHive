e557362e64efa73c5c1503b522de2e91e7eaff0e
Aman Raj Rana <mail2amanrana@gmail.com>
2023-09-27 19:44:48 +0530

    feat: Implement "Load More" functionality for contributors
    Issue #352:
    
    Added the "Load More" feature to the contributor page, allowing users to load additional contributors beyond the initial set. This enhancement is in line with the feature branch "feature/add-load-more-contributors."

--- web/src/pages/Contributors/ContributorsList.jsx
+++ web/src/pages/Contributors/ContributorsList.jsx
@@ -1,5 +1,5 @@
 import ContributorItem from './ContributorItem';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ScaleLoader from 'react-spinners/ScaleLoader';
 import SearchTerm from './search/Search';
 
@@ -9,54 +9,80 @@ const ContributorsList = () => {
   const [contributors, setContributors] = useState([]);
   const [loading, setLoading] = useState(true);
   const [search, setSearch] = useState('');
+  const [pageNo, setPageNo] = useState(1); 
+  const [isMoreContributors, setIsMoreContributors] = useState(true); // Flag to check if there are more contributors to fetch
+  const isInitialRender = useRef(true); // Initial render flag
 
   useEffect(() => {
     const fetchContributors = async () => {
       setLoading(true);
       try {
         const response = await axios.get(
-          'https://api.github.com/repos/ArslanYM/StarterHive/contributors',
+          `https://api.github.com/repos/ArslanYM/StarterHive/contributors?page=${pageNo}`,
           {
             headers: {
               Authorization: ``,
             },
           }
         );
-        setContributors(response.data);
+        setContributors((prev) => prev.concat(response.data));
+        setIsMoreContributors(response.data.length === 30);
       } catch (error) {
         console.error(error);
       }
       setLoading(false);
     };
 
-    fetchContributors();
-  }, []);
+    // Check if it's not the initial render before fetching data
+    if (!isInitialRender.current) {
+      fetchContributors();
+    } else {
+      // Set the initial render flag to false
+      isInitialRender.current = false;
+    }
+  }, [pageNo]);
 
   const filtered = contributors.filter((entry) =>
     entry.login.toLowerCase().match(new RegExp(search.toLowerCase(), 'g'))
   );
 
   return (
-    <div className="flex flex-wrap -m-2 justify-center items-center">
-      <ScaleLoader loading={loading} color="white" />
+    <>
+      <div className="flex flex-wrap -m-2 justify-center items-center">
+        <ScaleLoader loading={loading} color="white" />
 
-      {!loading ? <SearchTerm setTerm={setSearch} /> : ''}
+        {!loading ? <SearchTerm setTerm={setSearch} /> : ''}
 
-      {filtered.length
-        ? filtered.map((contributor, index) => {
-            return (
-              <ContributorItem
-                image={contributor.avatar_url}
-                name={contributor.login}
-                url={contributor.html_url}
-                key={index}
-              />
-            );
-          })
-        : loading
-        ? ''
-        : `No result for ${search}`}
-    </div>
+        {filtered.length
+          ? filtered.map((contributor, index) => {
+              return (
+                <ContributorItem
+                  image={contributor.avatar_url}
+                  name={contributor.login}
+                  url={contributor.html_url}
+                  key={index}
+                />
+              );
+            })
+          : loading
+          ? ''
+          : `No result for ${search}`}
+      </div>
+      <div className="flex w-full justify-center items-center mt-12 my-8">
+        {isMoreContributors ? (
+          !loading && (
+            <span
+              className="hover:text-white border border-yellow-400 cursor-pointer px-4 py-2 rounded-full"
+              onClick={() => setPageNo((prev) => prev + 1)}
+            >
+              Load More...
+            </span>
+          )
+        ) : (
+          <span className="italic">No more contributors</span>
+        )}
+      </div>
+    </>
   );
 };
 
