0c2d4a35670c9063f23804e85429797c53b20551
Dhoni77 <aldrinmarz7@gmail.com>
2023-11-01 14:17:59 +0530

    chore: format code

--- web/src/pages/Issues/IssuesPage/IssueNotFound.jsx
+++ web/src/pages/Issues/IssuesPage/IssueNotFound.jsx
@@ -1,8 +1,9 @@
-
 const IssueNotFound = () => {
   return (
     <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div className="text-red-600 title-font text-4xl text-center font-extrabold">No Issues found</div>
+      <div className="text-red-600 title-font text-4xl text-center font-extrabold">
+        No Issues found
+      </div>
     </div>
   );
 };
